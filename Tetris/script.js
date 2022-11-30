86119865d255e764ceca9047e208e0cb73ff975e
ted-jv <wkejrdldls@gmail.com>
2022-12-01 00:09:47 +0900

    Refactor: 변경 - 기존 배경 ul, li 만드는 기능 함수화
    시작 시 init() 호출

--- Tetris/script.js
+++ Tetris/script.js
@@ -1,11 +1,23 @@
+// DOM
 const playground = document.querySelector('.playground > ul');
 
-console.info(playground);
+// Setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
 
-for (let i = 0; i < 20; i++) {
+init();
+// functions
+
+function init() {
+  for (let i = 0; i < GAME_ROWS; i++) {
+    prependNewLine();
+  }
+}
+
+function prependNewLine() {
   const li = document.createElement('li');
   const ul = document.createElement('ul');
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < GAME_COLS; j++) {
     const matrix = document.createElement('li');
     ul.prepend(matrix);
   }
