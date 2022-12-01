cc427aec341092f26f70fbac4107e26f7261d498
ted-jv <wkejrdldls@gmail.com>
2022-12-01 22:16:01 +0900

    Feat: 추가 - 좌표 백그라운드 위에 테트리스 블록 생성

--- Tetris/script.js
+++ Tetris/script.js
@@ -5,13 +5,43 @@ const playground = document.querySelector('.playground > ul');
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
 
+// variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+  tree: [
+    [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [],
+    [],
+    [],
+  ],
+};
+
+const movingItem = {
+  type: 'tree',
+  direction: 0,
+  top: 0,
+  left: 0,
+};
+
 init();
+
 // functions
 
 function init() {
+  tempMovingItem = { ...movingItem };
   for (let i = 0; i < GAME_ROWS; i++) {
     prependNewLine();
   }
+  renderBlocks();
 }
 
 function prependNewLine() {
@@ -23,4 +53,14 @@ function prependNewLine() {
   }
   li.prepend(ul);
   playground.prepend(li);
+}
+
+function renderBlocks() {
+  const { type, direction, top, left } = movingItem;
+  BLOCKS[type][direction].forEach(block => {
+    const x = block[0] + left;
+    const y = block[1] + top;
+    const target = playground.childNodes[y].childNodes[0].childNodes[x];
+    target.classList.add(type);
+  });
 }
